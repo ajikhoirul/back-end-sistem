c8ecd7db3f07c6a106b78dd851a78f10ff5ec0cc
ajikhoirul <khoirulaji071@gmail.com>
2023-06-01 17:26:55 +0700

    update ke - 3

--- routes.js
+++ routes.js
@@ -3,14 +3,14 @@ const routes = [
     method: "*",
     path: "/",
     handler: (request, h) => {
-      return "Halaman tidak dapat diakses dengan method tersebut";
+      return "Halaman tidak dapat diakses dengan method tersebut update-2";
     },
   },
   {
     method: "GET",
     path: "/",
     handler: (request, h) => {
-      return "Homepage";
+      return "Homepage update 3";
     },
   },
 ];
